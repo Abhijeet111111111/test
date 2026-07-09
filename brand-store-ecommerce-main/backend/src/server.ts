be7ce2550f3ad247c63131b8b71c6eda4f620a2e
Abhijeet <yabhijeet083@gmail.com>
2026-07-09 13:57:15 +0530

    Update server.ts

--- brand-store-ecommerce-main/backend/src/server.ts
+++ brand-store-ecommerce-main/backend/src/server.ts
@@ -33,7 +33,7 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: "https://test-one-phi-94.vercel.app" || 'http://localhost:3000',
+  origin: "https://test-one-phi-94.vercel.app",
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
