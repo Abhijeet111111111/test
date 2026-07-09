95d54c5663735efd263ffa5adac9dd7ace2073f7
Abhijeet <yabhijeet083@gmail.com>
2026-07-09 13:51:20 +0530

    Update server.ts

--- brand-store-ecommerce-main/backend/src/server.ts
+++ brand-store-ecommerce-main/backend/src/server.ts
@@ -33,7 +33,7 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: "https://brand-store-ecommerce-f998.onrender.com" || 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
